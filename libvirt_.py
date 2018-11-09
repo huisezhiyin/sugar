92a47bfd88157410167f8438acfe72dac1c35e89
GreyChen <837364695@qq.com>
2018-11-09 18:17:39 +0800

    Update libvirt_.py

--- libvirt_.py
+++ libvirt_.py
@@ -15,35 +15,39 @@ for i in conn.listDomainsID():
     c2 = int(dom.info()[4])
     c_nums = int(dom.info()[3])
     usage = (c2 - c1) * 100 / ((t2 - t1) * c_nums * 1e9)
-    # print dom.name(), "{0}%".format(usage)
     data[dom.name()]["cpu"] = "{0}%".format(usage)
+
     # memory
     meminfo = dom.memoryStats()
     free_mem = float(meminfo['unused'])
     total_mem = float(meminfo['available'])
     util_mem = ((total_mem - free_mem) / total_mem) * 100
-    # print (str(domain.name()) + ' Memory usage :' + str(util_mem))
     data[dom.name()]["memory"] = "{0}%".format(util_mem)
 
     tree = ElementTree.fromstring(dom.XMLDesc())
-    i_faces = tree.findall('devices/interface/target')
-    for j in i_faces:
-        iface = j.get('dev')
-        # net
-        ifaceinfo = dom.interfaceStats(iface)
-        print dom.name(), iface, ifaceinfo
-        read_1 = ifaceinfo[0]
-        write_1 = ifaceinfo[4]
+    # net
+    for tmp_net in tree.findall('devices/interface/target'):
+        dev = tmp_net.get('dev')
+        network_info = dom.interfaceStats(dev)
+        read_bytes1 = network_info[0]
+        write_bytes1 = network_info[4]
         time.sleep(1)
-        ifaceinfo_2 = dom.interfaceStats(iface)
-        read_2 = ifaceinfo_2[0]
-        write_2 = ifaceinfo_2[4]
+        network_info = dom.interfaceStats(dev)
+        read_bytes2 = network_info[0]
+        write_bytes2 = network_info[4]
+        write = write_bytes2 - write_bytes1
+        read = read_bytes2 - read_bytes1
 
-    tree = ElementTree.fromstring(dom.XMLDesc())
-    devices = tree.findall('devices/disk/target')
-    for d in devices:
-        device = d.get('dev')
-        devstats = dom.blockStats(device)
-        print dom.name(), device, devstats
-        rd_bytes = devstats[1]
-        wr_bytes = devstats[3]
+
+    # disk
+    for tmp_disk in tree.findall('devices/disk/target'):
+        dev = tmp_disk.get('dev')
+        disk_info = dom.blockStats(dev)
+        read_bytes1 = disk_info[1]
+        write_bytes1 = disk_info[3]
+        time.sleep(1)
+        disk_info = dom.blockStats(dev)
+        read_bytes2 = disk_info[1]
+        write_bytes2 = disk_info[3]
+        write = write_bytes2 - write_bytes1
+        read = read_bytes2 - read_bytes1
